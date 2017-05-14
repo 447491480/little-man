a0ce4040e661f993ecc05f4968a5b3410d865464
Your风之恋 <yoursfengzhilian@gmail.com>
2017-05-14 23:47:32 +0800

    add jggrid bootstrap ui

--- public/bootstrap/js/webapp/admin/page/admin_reset_password.js
+++ public/bootstrap/js/webapp/admin/page/admin_reset_password.js
@@ -1,3 +1,26 @@
 /**
  * Created by jhzhang on 2017/5/12.
  */
+layui.use(['form', 'ajaxUtil'], function(){
+    var form = layui.form();
+    var ajaxUtil = layui.ajaxUtil;
+    var commonUtil = layui.commonUtil;
+    var $ = layui.jquery;
+
+    var urls = {};
+    urls.reset_password = '/admin/user/reset-password';
+
+    //监听提交
+    form.on('submit(reset_password)', function(data){
+        ajaxUtil.doAjaxGet(urls.reset_password,data.field).done(function(ret) {
+            if(ret.status == 0) {
+                $('#resetPwdBtn').trigger('click');
+
+                commonUtil.toast(ret.msg);
+            } else {
+                commonUtil.toast(ret.msg);
+            }
+        });
+        return false;
+    });
+});
