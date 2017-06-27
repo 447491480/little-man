b8395d47921a2e42fe0c77814df74f9d9b89aa86
447491480@qq.com <447491480@qq.com>
2017-06-27 13:42:08 +0800

    1 update app.js

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 // 载入常用nodejs模块
-var path = require('path');
-var fs = require('fs');
-var schedule = require('node-schedule');
+const path = require('path');
+const fs = require('fs');
 
-var configure = require('little-man-config');
+// 定时任务模块
+const schedule = require('node-schedule');
+
+//配置管理
+const configure = require('little-man-config');
 
 // 载入app配置文件
-var app_config = configure.get('app');
+const app_config = configure.get('app');
 
 // favicon.ico 支持
-var favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
+
 // 日志输出
-var fileStreamRotator = require('file-stream-rotator');
-var logger = require('morgan');
+const fileStreamRotator = require('file-stream-rotator');
+const logger = require('morgan');
+
 // post参数解析
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
 // flash支持
-var flash = require('connect-flash');
-var ejs = require('ejs');
+const flash = require('connect-flash');
+
+// ejs
+const ejs = require('ejs');
 
 // express 框架
-var express = require('express');
-var app = express();
+const express = require('express');
 
+const app = express();
 app.use(function (req, res, next) {
     res.jsonWrap = function (data, status, msg) {
         status = status || 0;
@@ -35,35 +43,30 @@ app.use(function (req, res, next) {
     next();
 });
 
-// 载入核心路由解析组件
-var resolve = require(path.join(__dirname, 'utils', 'route'));
-// 载入log4js自定义模块
-var log4js = require(path.join(__dirname, 'utils', 'log4js'));
-// cookie
-var cookieParser = require('cookie-parser');
-// session
-var session = require('express-session');
-var fileSession =require('session-file-store')(session);
-// cors
-var cors = require('cors');
 // 将db对象载入全局对象中
-global.db = require(path.join(__dirname, 'utils', 'db'));
-global.pager = require(path.join(__dirname, 'utils', 'pager'));
-global.helper = require(path.join(__dirname, 'utils', 'helper'));
+global.APP_PATH = process.cwd();
+global.db = require(path.join(APP_PATH, 'utils', 'db'));
+global.pager = require(path.join(APP_PATH, 'utils', 'pager'));
+global.helper = require(path.join(APP_PATH, 'utils', 'helper'));
 
-log4js.configure();
-app.use(log4js.useLog());
+// 载入核心路由解析组件
+const resolve = require(path.join(APP_PATH, 'utils', 'route'));
+// 载入log4js自定义模块
+const log4js = require(path.join(APP_PATH, 'utils', 'log4js'));
+// cookie
+const cookieParser = require('cookie-parser');
 
-// 跨域支持
-app.use(cors());
+// session
+const session = require('express-session');
+const fileSession = require('session-file-store')(session);
 
-// 会话处理
 // cookie处理
 app.use(cookieParser());
+
 // session处理
 app.use(session({
     store: new fileSession({
-        ttl:app_config.cookie_maxAge/1000,
+        ttl: app_config.cookie_maxAge / 1000,
         secret: app_config.session_secret
     }),
     secret: app_config.session_secret,
@@ -72,15 +75,25 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// 跨域支持
+const cors = require('cors');
+app.use(cors());
+
+
+// 配置log4js
+log4js.configure();
+app.use(log4js.useLog());
+
 // 设置视图引擎
 app.engine('html', ejs.__express);
 app.set('view engine', 'html');
 
 // 设置静态资源目录
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(APP_PATH, 'public')));
 
 // 设置icon图标（如果没有favicon.icon）可以注释这一行代码
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(APP_PATH, 'public', 'favicon.ico')));
+
 //flash支持
 app.use(flash());
 
@@ -89,18 +102,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 // 设置默认区域
-var defaultArea = app_config.default_area;
+const defaultArea = app_config.default_area;
 
 // 创建express Router 实例
-var router = express.Router();
+const router = express.Router();
 
 // 定义视图文件位置
-app.set('views', path.join(__dirname, 'public', app_config['default_theme'], 'webapp'));
+app.set('views', path.join(APP_PATH, 'public', app_config['default_theme'], 'webapp'));
 
 // 实现ViewData全局功能，
 // ejs页面通过 <%=_locals.（locals文件夹下的文件名） %>访问，如：<%=_locals.setting.name %>
 // action通过 res.locals.setting  访问
-var locals = require(path.join(__dirname, "utils", "locals"));
+const locals = require(path.join(APP_PATH, "utils", "locals"));
 router.use(function (req, res, next) {
     res.locals = locals;
     next();
@@ -110,14 +123,14 @@ router.use(function (req, res, next) {
 app.use(router);
 
 // 日志输出到文件系统，每日一个日志文件
-var logDirectory = __dirname + '/logs';
+const logDirectory = APP_PATH + '/logs';
 fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
-var accessLogDirectory = __dirname + '/logs/access';
+const accessLogDirectory = APP_PATH + '/logs/access';
 fs.existsSync(accessLogDirectory) || fs.mkdirSync(accessLogDirectory);
-var errorLogDirectory = __dirname + '/logs/error';
+const errorLogDirectory = APP_PATH + '/logs/error';
 fs.existsSync(errorLogDirectory) || fs.mkdirSync(errorLogDirectory);
 // 保存访问日志
-var accessLogStream = fileStreamRotator.getStream({
+const accessLogStream = fileStreamRotator.getStream({
     filename: accessLogDirectory + '/access-%DATE%.log',
     frequency: 'daily',
     verbose: false,
@@ -125,12 +138,12 @@ var accessLogStream = fileStreamRotator.getStream({
 });
 app.use(logger('combined', {stream: accessLogStream}));
 // 设置错误日志文件地址
-var errorLogStream = fs.createWriteStream(errorLogDirectory + '/error.log', {'flags': 'a'});
+const errorLogStream = fs.createWriteStream(errorLogDirectory + '/error.log', {'flags': 'a'});
 
 // 设置控制器文件夹并绑定到路由
 resolve
     .setRouteDirectory({
-        areaDirectory: __dirname + '/areas',
+        areaDirectory: APP_PATH + '/areas',
         controllerDirname: 'controllers',
         defaultArea: defaultArea,
         defautController: app_config.default_controller,
@@ -141,42 +154,42 @@ resolve
 // 错误处理
 // 404处理
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
 // 错误或者服务器500异常处理
 app.use(function (err, req, res, next) {
-    var error = (req.app.get('env') === 'development') ? err : {};
+    let error = (req.app.get('env') === 'development') ? err : {};
     //写错误日志
-    var errorMes = '[' + Date() + ']' + req.url + '\n' + '[' + error.stack + ']' + '\n';
+    let errorMes = '[' + Date() + ']' + req.url + '\n' + '[' + error.stack + ']' + '\n';
     errorLogStream.write(errorMes);
-    var status = err.status || 500;
+    let status = err.status || 500;
     res.status(status);
     res.send('<pre>' + status + ' ' + err.message + '\n' + errorMes + '</pre>');
 });
 
 // 设置端口
 app.set('port', process.env.PORT || app_config.default_port);
-var server = app.listen(app.get('port'), function () {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(app.get('port'), function () {
+    let host = server.address().address;
+    let port = server.address().port;
     console.log('应用启动成功！访问地址： http://%s:%s', host, port);
 });
 
 // 初始化定时任务
-var taskDir = __dirname + '/tasks';
+const taskDir = APP_PATH + '/tasks';
 fs.existsSync(taskDir) || fs.mkdirSync(taskDir);
 fs.readdirSync(taskDir)
     .filter(function (file) {
         return (path.extname(file) === '.js');
     })
     .forEach(function (file) {
-       var job = require(path.join(taskDir,file));
-       schedule.scheduleJob(job.cron,job.func);
+        let job = require(path.join(taskDir, file));
+        schedule.scheduleJob(job.cron, job.func);
     });
 
 
 // 引入socket 服务端模块。如无需即时通讯，注释即可
-require(path.join(__dirname, 'utils', 'socket')).listen(server);
+require(path.join(APP_PATH, 'utils', 'socket')).listen(server);
